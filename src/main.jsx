407a14fbd0f7a4c2a3860a735cf7e5c582dd5ce4
cristian villate <cristian_17v@hotmail.com>
2022-06-29 20:36:10 -0500

    video 124

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
@@ -10,8 +11,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   </React.StrictMode>
 )
-
 
