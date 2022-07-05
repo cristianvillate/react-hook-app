a74f6ceac21f4eb4db9f9d4f63b958717deb0359
cristian villate <cristian_17v@hotmail.com>
2022-07-05 16:53:55 -0500

    tarea

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,3 +1,37 @@
 
 
-console.log("hola mundo")
+const initialState = [{
+    id: 1,
+    todo: 'Recolectar la piedra del alma',
+    done: false,
+
+}];
+
+const todoReducer = (state = initialState, action = {}) => {
+
+if (action.type === 'ADD_TODO') {
+    return [...state, action.payload];
+}
+    
+
+
+return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id:2,
+    todo:'Recolectar el oro del alma',
+    done:false,
+}
+
+const addTodoAction = {
+    type: 'ADD_TODO',
+    payload: newTodo,
+}
+
+todos = todoReducer(todos,addTodoAction);
+
+
+console.log({state: todos});
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-import { Padre } from './07-tarea-memo/Padre';
+// import { Padre } from './07-tarea-memo/Padre';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
-import "./08-useReducer/intro-reducer";
+// import "./08-useReducer/intro-reducer";
+import { TodoApp } from "./08-useReducer/TodoApp";
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <TodoApp />
+//  </React.StrictMode>
 )
 
