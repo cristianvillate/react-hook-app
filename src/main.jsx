d7c57eb344159848f26aec84c16e1a57a6e2ff3f
cristian villate <cristian_17v@hotmail.com>
2022-07-05 13:43:57 -0500

    prueba

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { Padre } from './07-tarea-memo/Padre';
 //import { SimpleForm } from './02-useEffect/SimpleForm';
 //import { CounterApp } from './01-useState/CounterApp';
 //import { HooksApp } from './HooksApp';
@@ -11,7 +12,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MultipleCustomHooks />
+    <Padre />
   </React.StrictMode>
 )
 
