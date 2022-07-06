6653f5a3446339eed39cf1bb2d59c4c3e888366b
cristian villate <cristian_17v@hotmail.com>
2022-07-06 16:21:47 -0500

    seccion-10-fin

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,63 +1,31 @@
-import { useReducer } from "react"
-import { todoReducer } from "./todoReducer"
 
-const initialState = [{
-  id: new Date().getTime(),
-  description: "recolectar la piedra del alma",
-  done: false,
-},
-{
-  id: new Date().getTime() * 3,
-  description: "recolectar la piedra del alma",
-  done: false,
-}
-]
-
-
+import { useTodos } from "../hooks";
+import { TodoAdd } from "./TodoAdd";
+import { TodoList } from "./TodoList";
 
 export const TodoApp = () => {
 
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
-
-  const handleNewTodo = (todo =>{
-    console.log({todo});
-  })
+  const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodos();
 
   return (
     <>
-      <h1>TodoApp 10, <small>pendientes: 2</small> </h1>
+      <h1>TodoApp {todosCount}, <small>pendientes: {pendingTodosCount}</small> </h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
 
-          <ul className="list-group">
-            {
-              todos.map(todo => (
-
-                <li key={todo.id} className="list-group-item d-flex justify-content-between">
-                  <span className="align-self-center">item 1</span>
-                  <button className="btn btn-danger">borrar</button>
-
-                </li>
-
-              ))
-            }
-
-
-          </ul>
+          <TodoList
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
 
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form action="">
-            <input type="text" placeholder="que hay que hacer"
-              className="form-control" />
-
-            <button type="submit"
-              className="btn btn-outline-primary mt-1">Agregar</button>
-          </form>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
 
